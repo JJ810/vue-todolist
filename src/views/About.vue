72aa99afa62fc90f9ea89b2242383e16fec3ee6a
Afon <hcls70810@gmail.com>
2020-09-21 21:50:58 +0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube</h1>
+  </div>
+</template>
